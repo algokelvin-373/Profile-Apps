66998ea17f3a3b9512e4bc5d6825ea9b7f9cbf02
kelvintandrio <kelvin.tandrio@tsmid.com>
2020-11-08 16:09:29 +0700

    Backend: First Commit

---
+++ Backend/profileapps/src/test/java/algokelvin/apps/profileapps/ProfileappsApplicationTests.java
@@ -0,0 +1,13 @@
+package algokelvin.apps.profileapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
