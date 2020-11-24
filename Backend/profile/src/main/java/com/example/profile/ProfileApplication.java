c49cc10fb2437959c9b0de719e9d3ca35bdd8dbb
kelvin373-ht <kelvinht373@gmail.com>
2020-11-24 22:08:20 +0700

    Backend: Succes connect DB

--- Backend/profile/src/main/java/com/example/profile/ProfileApplication.java
+++ Backend/profile/src/main/java/com/example/profile/ProfileApplication.java
@@ -1,13 +1,14 @@
 package com.example.profile;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class ProfileApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProfileApplication.class, args);
+		run(ProfileApplication.class, args);
 	}
 
 }
